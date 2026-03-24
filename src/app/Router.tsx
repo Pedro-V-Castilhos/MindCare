5bce63489d68feb92a3cf2e4dcb490267177a662
Pedro Viana de Castilhos <pedro.viana.castilhos@gmail.com>
2026-03-23 21:54:28 -0300

    :boom: fix: Correção referente arquivos App.tsx e Router.tsx

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router"
-import PacientDashboard from "./components/screens/pacient/Dashboard"
-import Login from "./components/screens/Login"
-import Appointments from "./components/screens/pacient/Appointments"
-import PacientProgress from "./components/screens/pacient/PacientProgress"
-import PacientNotes from "./components/screens/pacient/Notes"
-import PacientDocuments from "./components/screens/pacient/Documents"
+import PacientDashboard from "../components/screens/pacient/Dashboard"
+import Login from "../components/screens/Login"
+import Appointments from "../components/screens/pacient/Appointments"
+import PacientProgress from "../components/screens/pacient/PacientProgress"
+import PacientNotes from "../components/screens/pacient/Notes"
+import PacientDocuments from "../components/screens/pacient/Documents"
 
 const router = createBrowserRouter([
   {
