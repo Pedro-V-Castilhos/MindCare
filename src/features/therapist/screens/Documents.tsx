f41e0c570ccc642feaa5870d6917d8466e70f4f3
Pedro Viana de Castilhos <pedro.viana.castilhos@gmail.com>
2026-04-01 14:11:36 -0300

    :sparkles: feat: Implementação do formulário de upload de documentos do terapeuta

--- src/features/therapist/screens/Documents.tsx
+++ src/features/therapist/screens/Documents.tsx
@@ -10,15 +10,149 @@ import { useSessionStore } from "@/hooks/sessionStore";
 import type { Document } from "@/types/document";
 import { downloadFile } from "@/lib/bucket";
 import { useUserStore } from "@/hooks/userStore";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Controller, useForm } from "react-hook-form";
+import { Combobox, ComboboxContent, ComboboxInput, ComboboxItem, ComboboxList } from "@/components/ui/combobox";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useShallow } from "zustand/shallow";
+
+interface DocumentFormData {
+    pacientId: number;
+    name: string;
+    type: "receipt" | "report" | "prescription" | "other";
+    file: FileList;
+}
 
 function TherapistDocuments() {
+    const session = useSessionStore((s) => s.session);
+    const user = session?.user as Therapist;
+    const patients = useUserStore(useShallow(state => state.users.filter(u => u.role === "pacient" && u.therapistId === user.id)));
+    const [open, setOpen] = useState(false);
+    const { register, handleSubmit, control, watch } = useForm<DocumentFormData>({
+        defaultValues: {
+            pacientId: 0,
+            name: "",
+            type: "receipt",
+            file: undefined,
+        }
+    });
+    const documentTypes: { [key: string]: string } = {
+        receipt: "Recibo",
+        report: "Relatório",
+        prescription: "Prescrição",
+        other: "Outro"
+    };
+    const file = watch("file");
+    const { documents, addDocument } = useDocumentsStore()
+
+    const submitNewDocument = async (data: DocumentFormData) => {
+        const file = data.file[0];
+        try {
+            addDocument({
+                id: documents.length > 0 ? Math.max(...documents.map(d => d.id)) + 1 : 1,
+                name: data.name,
+                type: data.type,
+                uploadDate: new Date(),
+                size: Math.round(file.size / 1024),
+                patientId: data.pacientId,
+                therapistId: user.id,
+            } as Document, file)
+            toast.success("Documento adicionado com sucesso!");
+        } catch (error) {
+            toast.error("Erro ao fazer upload do documento:" + (error instanceof Error ? error.message : "Erro desconhecido"));
+        } finally {
+            setOpen(false);
+        }
+    }
+
     return (
         <Layout>
             <div className="flex flex-row justify-between items-center">
                 <PageHeader>Documentos</PageHeader>
-                <Button onClick={() => useDocumentsStore.getState().reset()} variant="default" className="bg-black text-white cursor-pointer">
-                    <Plus className="mr-2" /> Adicionar Documento
-                </Button>
+                <Dialog open={open} onOpenChange={setOpen}>
+                    <DialogTrigger asChild>
+                        <Button variant="default" className="bg-black text-white cursor-pointer">
+                            <Plus className="mr-2" /> Adicionar Documento
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-lg bg-white backdrop:fill-accent-foreground">
+                        <form onSubmit={handleSubmit(submitNewDocument)}>
+                            <DialogHeader>
+                                <DialogTitle className="text-black">Fazer Upload de Documento</DialogTitle>
+                                <DialogDescription>Adicione um novo documento ao sistema</DialogDescription>
+                            </DialogHeader>
+                            <FieldGroup className="py-4">
+                                <Field>
+                                    <FieldLabel className="text-black">Paciente:</FieldLabel>
+                                    <Controller control={control} name="pacientId" rules={{ required: true }} render={({ field }) => (
+                                        <Combobox items={patients} onValueChange={(value) => field.onChange(Number(value))} value={field.value} itemToStringLabel={(value) => {
+                                            const item = patients.find(u => u.id === value);
+                                            return item ? `${item.firstName} ${item.lastName}` : "";
+                                        }}>
+                                            <ComboboxInput placeholder="Selecione o paciente..." className="z-50 w-full sm:w-auto text-black! bg-gray-100! border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" />
+                                            <ComboboxContent>
+                                                <ComboboxList className="pointer-events-auto">
+                                                    {(item) => (
+                                                        <ComboboxItem
+                                                            key={item.id}
+                                                            value={item.id}
+                                                            className="cursor-pointer px-3 py-2 rounded-md hover:bg-gray-200"
+                                                        >
+                                                            {item.firstName} {item.lastName}
+                                                        </ComboboxItem>
+                                                    )}
+                                                </ComboboxList>
+                                            </ComboboxContent>
+                                        </Combobox>
+                                    )} />
+
+                                </Field>
+                                <Field>
+                                    <FieldLabel className="text-black">Nome do Documento:</FieldLabel>
+                                    <Input {...register("name", { required: true })} placeholder="Ex: Recibo - Sessão --/--/----" className="text-black w-full bg-gray-100! placeholder:text-muted-foreground! border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" />
+                                </Field>
+                                <Field>
+                                    <FieldLabel className="text-black">Tipo do Documento:</FieldLabel>
+                                    <Controller control={control} name="type" rules={{ required: true }} render={({ field }) => (
+                                        <Combobox items={Object.keys(documentTypes)} onValueChange={field.onChange} value={field.value ?? ""} itemToStringLabel={(value) => documentTypes[value]}>
+                                            <ComboboxInput placeholder="Selecione o tipo de documento" className="z-50 w-full sm:w-auto text-black! bg-gray-100! border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" />
+                                            <ComboboxContent>
+                                                <ComboboxList className="pointer-events-auto">
+                                                    {(item) => (
+                                                        <ComboboxItem
+                                                            key={item}
+                                                            value={item}
+                                                            className="cursor-pointer px-3 py-2 rounded-md hover:bg-gray-200"
+                                                        >
+                                                            {documentTypes[item]}
+                                                        </ComboboxItem>
+                                                    )}
+                                                </ComboboxList>
+                                            </ComboboxContent>
+                                        </Combobox>
+                                    )} />
+                                </Field>
+                                <Field>
+                                    <FieldLabel className="text-black">Arquivo:</FieldLabel>
+                                    <Input id="file" {...register("file", { required: true, max: { value: 52428800, message: "O arquivo deve ter no máximo 50MB" } })} type="file" accept=".pdf,.doc,.docx,.txt,.jpg,.png,.jpeg" className="hidden" />
+                                    <label htmlFor="file" className="w-full cursor-pointer bg-gray-100 border border-gray-300 rounded-md px-3 py-2 text-black hover:bg-gray-200">
+                                        <FileText className="inline-block mr-2" />{file && file.length > 0 ? file[0].name : "Selecione um arquivo"}
+                                    </label>
+                                    <FieldDescription className="text-sm text-muted-foreground">Tipos permitidos: PDF, DOC, DOCX, TXT, JPG, PNG, JPEG (Max: 50MB)</FieldDescription>
+                                </Field>
+                            </FieldGroup>
+                            <DialogFooter className="bg-white">
+                                <DialogClose asChild>
+                                    <Button variant="outline" className="border-s! border-gray-300! text-black hover:text-black! hover:bg-accent-foreground! cursor-pointer">Cancelar</Button>
+                                </DialogClose>
+                                <Button type="submit" className="bg-indigo-700 hover:bg-indigo-800 text-white cursor-pointer">Agendar</Button>
+                            </DialogFooter>
+                        </form>
+                    </DialogContent>
+                </Dialog>
             </div>
             <DocumentList />
         </Layout>
